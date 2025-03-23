eb3a31e2d64279571df6ab9046adec969f87a9a3
eduardoranucci <eduardoranucci@outlook.com>
2025-03-23 15:47:03 -0300

    feat: adiciona verificação de autenticação nas rotas de login via middleware

--- app/Http/Middleware/VerificaNaoAutenticado.php
+++ app/Http/Middleware/VerificaNaoAutenticado.php
@@ -6,18 +6,18 @@ use Closure;
 use Illuminate\Http\Request;
 use Symfony\Component\HttpFoundation\Response;
 
-class VerificaAutenticacao {
+class VerificaNaoAutenticado {
     /**
      * Handle an incoming request.
      *
      * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
      */
     public function handle(Request $request, Closure $next): Response {
-        // verifica se o usuario está autenticado
-        if(!session('usuario')) {
-            return redirect('/login');
+        // verifica se o usuario não está autenticado
+        if(session('usuario')) {
+            return redirect('/');
         }
-
+        
         return $next($request);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -3,15 +3,18 @@
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\MainController;
 use App\Http\Middleware\VerificaAutenticacao;
+use App\Http\Middleware\VerificaNaoAutenticado;
 use Illuminate\Support\Facades\Route;
 
-// rotas de auth
-Route::get('/login', [AuthController::class, 'login']);
-Route::post('/loginSubmit', [AuthController::class, 'loginSubmit']);
+Route::middleware([VerificaNaoAutenticado::class])->group( function() {
+    // rotas de auth
+    Route::get('/login', [AuthController::class, 'login']);
+    Route::post('/loginSubmit', [AuthController::class, 'loginSubmit']);
+});
 
 Route::middleware([VerificaAutenticacao::class])->group( function() {
     // rotas principais
     Route::get('/', [MainController::class, 'index']);
     Route::get('/novaNota', [MainController::class, 'novaNota']);
     Route::get('/logout', [AuthController::class, 'logout']);
-}) ;
+});
