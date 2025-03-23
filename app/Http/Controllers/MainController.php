9b367b8a3643cb21b020769ddeea9591d5a24666
eduardoranucci <eduardoranucci@outlook.com>
2025-03-23 15:34:46 -0300

    feat: adiciona verificação de autenticação nas rotas necessárias via middleware

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -6,4 +6,12 @@ use Illuminate\Http\Request;
 
 class MainController extends Controller {
     
+    public function index() {
+        echo 'dentro do app!';
+    }
+
+    public function novaNota() {
+        echo 'criando nova nota';
+    }
+
 }
--- routes/web.php
+++ routes/web.php
@@ -2,9 +2,16 @@
 
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\MainController;
+use App\Http\Middleware\VerificaAutenticacao;
 use Illuminate\Support\Facades\Route;
 
 // rotas de auth
 Route::get('/login', [AuthController::class, 'login']);
 Route::post('/loginSubmit', [AuthController::class, 'loginSubmit']);
-Route::get('/logout', [AuthController::class, 'logout']);
+
+Route::middleware([VerificaAutenticacao::class])->group( function() {
+    // rotas principais
+    Route::get('/', [MainController::class, 'index']);
+    Route::get('/novaNota', [MainController::class, 'novaNota']);
+    Route::get('/logout', [AuthController::class, 'logout']);
+}) ;
