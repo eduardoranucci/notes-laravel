f0cd8d53aaaf4cdf7dce9e3573e45db11087651d
eduardoranucci <eduardoranucci@outlook.com>
2025-03-23 22:03:49 -0300

    feat: cria rotas de editar e deletar

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -3,7 +3,9 @@
 namespace App\Http\Controllers;
 
 use App\Models\Usuario;
+use Illuminate\Contracts\Encryption\DecryptException;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Crypt;
 
 class MainController extends Controller {
     
@@ -19,4 +21,25 @@ class MainController extends Controller {
         echo 'criando nova nota';
     }
 
+    public function edita($id) {
+
+        try {
+            $id = Crypt::decrypt($id);
+        } catch (DecryptException $e) {
+            return redirect()->route('home');
+        }
+
+        echo "editando a nota $id";
+    }
+
+    public function deleta($id) {
+
+        try {
+            $id = Crypt::decrypt($id);
+        } catch (DecryptException $e) {
+            return redirect()->route('home');
+        }
+
+        echo "deletando a nota $id";
+    }
 }
